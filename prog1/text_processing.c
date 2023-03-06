c76e4b07e900f0372d564ae16d4258bcf3bb8caf
Dinis-Lei <dinislei@ua.pt>
2023-03-06 20:47:59 +0000

    added command line parsing

--- prog1/text_processing.c
+++ prog1/text_processing.c
@@ -7,6 +7,7 @@
 #include <math.h>
 #include <time.h>
 
+static void print_usage (char *cmdName);
 
 // TODO: specify monitor's data structures and the routines to access it
 
@@ -17,11 +18,43 @@ struct {
 
 int main (int argc, char *argv[]) {
 
-    if (argc < 2) {
-        printf("Input some files to process!\n");
-        return 0;
+    int opt;
+    while((opt = getopt(argc, argv, ":h")) != -1) {
+        switch (opt) {
+            case 'h':
+                print_usage(basename (argv[0]));
+                return EXIT_SUCCESS;
+            case '?': /* invalid option */
+                fprintf (stderr, "%s: invalid option\n", basename (argv[0]));
+                return EXIT_FAILURE;
+        }
     }
 
+
+    if (argc < 2) {
+        fprintf(stderr, "Input some files to process!\n");
+        return EXIT_FAILURE;
+    }
+	
+	for(; optind < argc; optind++){	
+		printf("File name: %s\n", argv[optind]);
+        //TODO: Add Files pointers to shared memory
+	}
+
     
-    return 0;
+    return EXIT_SUCCESS;
+}
+
+/**
+ *  \brief Print command usage.
+ *
+ *  A message specifying how the program should be called is printed.
+ *
+ *  \param cmdName string with the name of the command
+ */
+static void print_usage (char *cmdName)
+{
+  fprintf (stderr, "\nSynopsis: %s OPTIONS [FILE]...\n"
+           "  OPTIONS:\n"
+           "  -h      --- print this help\n", cmdName);
 }
