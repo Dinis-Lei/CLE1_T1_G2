35f441c0d72c258cda89a555d637066a7581b20f
Dinis-Lei <dinislei@ua.pt>
2023-03-13 13:04:35 +0000

    class changes

--- prog1/text_processing.c
+++ prog1/text_processing.c
@@ -12,6 +12,7 @@
 #define  MAX_CHUNK_SIZE_MIN  4
 #define  MAX_CHUNK_SIZE_MAX  8
 #define  N_VOWELS  7
+#define  MAX_CHUNK_SIZE  8
 
 /**
  *  \brief Print command usage.
@@ -23,19 +24,23 @@
 static void print_usage (char *cmdName);
 
 // TODO: main should wait for each file's results, or only wait at the end for everything?
+// TODO: can we simplify and create/terminate threads at each processed file?
 
 /**
  * @brief Read a fixed-size chunk of text from the file being currently read.
  * 
  * Performed by the worker threads.
+ * The maximum number of bytes read is defined in the macro MAX_CHUNK_SIZE.
  * Reads the file_pointer in shared memory, and so should have exclusive access.
  * The actual amount of bytes read varies, since the file is only read until no word or UTF-8 character is cut.
- *
+ * 
+ * @param worker_id application-defined ID of the calling worker thread
  * @param chunk address in memory where the chunk should be read to
- * @param chunk_size size of the allocated memory for the chunk (in bytes)
- * @return int the number of bytes actually read from the file
+ * @param file_id the application-defined ID of the file that the chunk is from.
+ * This value is overwritten, and should merely be passed to the updateCounters() function in the future
+ * @return the number of bytes actually read from the file
  */
-int readChunk(char* chunk, int* chunk_size);
+int readChunk(unsigned int worker_id, unsigned char* chunk, int* file_id);
 
 /**
  * @brief Update the word counts of the file being currently read.
@@ -43,21 +48,28 @@ int readChunk(char* chunk, int* chunk_size);
  * Performed by the worker threads.
  * Writes to the counters in shared memory, and so should have exclusive access.
  *
- * @param counters array with the counter increments, in the same order as the shared counters
+ * @param worker_id application-defined ID of the calling worker thread
+ * @param worker_counters array with the counter increments, in the same order as the shared counters
+ * @param file_id the file associated with the passed word counts
  */
-void updateCounters(int* counters);
+void updateCounters(unsigned int worker_id, int* worker_counters, int* file_id);
 
 /**
  * @brief Process the recently read chunk, updating the word counts.
  * 
  * Performed by the worker threads.
  *
- * @param counters array of counters to be overwritten with the chunk's word counts
+ * @param chunk the chunk of text to calculate word counts
+ * @param chunk_size number of bytes to be read from the chunk
+ * @param worker_counters array of counters to be overwritten with the chunk's word counts
  */
-void processText(char* chunk, int chunk_size, int* counters);
+void processText(char* chunk, int chunk_size, int* worker_counters);
 
-/** @brief Print the formatted word count results to standard output */
-void printResults();
+/**
+ * @brief Print the formatted word count results to standard output
+ * @param file_names Array with the name of the files to show results
+ */
+void printResults(char** file_names);
 
 /** @brief Worker threads' function, which will concurrently update the word counters for the file in file_pointer */
 void *worker(void *id);
@@ -67,7 +79,11 @@ void *worker(void *id);
 static bool work_done = false;
 /** @brief True when the main thread knows the current file has been completely read. Worker threads wait when this value becomes true */
 static bool file_done = false;
-/** @brief Number of threads to be run in the program. Can be changed with command-line arguments, and it's global as the worker threads need to be aware of how many there are */
+/** @brief Number of files to be processed */
+static int n_files;
+/** @brief Number of threads to be run in the program. Can be changed with command-line arguments, and it's global 
+ * as the worker threads need to be aware of how many there are 
+ */
 static int n_threads = 4;
 /** @brief Size of the chunks to be read by the workers, in kilobytes. Can be changed with command-line arguments */
 static int max_chunk_size = 4;
@@ -75,21 +91,29 @@ static int max_chunk_size = 4;
 static int* statusWorkers;
 
 // State Control variables
-/** @brief Workers synchronization point for new work to be assigned */
-static pthread_cond_t await_work;
-/** @brief Workers synchronization point for new work to be assigned */
-static pthread_cond_t await_results;
 /** \brief Locking flag which warrants mutual exclusion inside the monitor */
 static pthread_mutex_t accessCR = PTHREAD_MUTEX_INITIALIZER;
 
 // Shared Region Variables
-/** @brief Array holding the word counts: [words, words with A, words with E, words with I, words with O, words with U, words with Y] */
-int counters[N_VOWELS];
-/** @brief Pointer representing the file currently being read */
-FILE *file_pointer;
+/** @brief Matrix holding the word counts: [words, words with A, words with E, words with I, words with O, words with U, words with Y] */
+int (*counters)[N_VOWELS];
+/** @brief Array of file names to be processed */
+char** file_names;
 /** @brief Counter of the number of threads that have finished processing the file chunks and updating the word counters */
-int finished_processing;
+int finished_processing = 0;
+/** @brief  Index of the current file being processed */
+int file_id;
 
+/**
+ *  \brief Main thread.
+ *
+ *  Its role is starting the processing by generating the workers threads and redistribute work for them.
+ *
+ *  \param argc number of words of the command line
+ *  \param argv list of words of the command line
+ *
+ *  \return status of operation
+ */
 int main (int argc, char *argv[]) {
 
     int opt;
@@ -111,23 +135,6 @@ int main (int argc, char *argv[]) {
                     return EXIT_FAILURE;
                 }
                 break;
-            case 's':
-                if (atoi(optarg) <= 0){ 
-                    fprintf(stderr, "%s: non positive number\n", basename(argv[0]));
-                    print_usage(basename(argv[0]));
-                    return EXIT_FAILURE;
-                }
-                max_chunk_size = (int) atoi(optarg);
-                if (max_chunk_size > MAX_CHUNK_SIZE_MAX){ 
-                    fprintf(stderr, "%s: chunk size is too large\n", basename(argv[0]));
-                    print_usage(basename(argv[0]));
-                    return EXIT_FAILURE;
-                }
-                if (max_chunk_size < MAX_CHUNK_SIZE_MIN){ 
-                    fprintf(stderr, "%s: chunk size is too small\n", basename(argv[0]));
-                    print_usage(basename(argv[0]));
-                    return EXIT_FAILURE;
-                }
             case 'h':
                 print_usage(basename(argv[0]));
                 return EXIT_SUCCESS;
@@ -136,7 +143,6 @@ int main (int argc, char *argv[]) {
                 return EXIT_FAILURE;
         }
     }
-
 
     if (argc < 2) {
         fprintf(stderr, "Input some files to process!\n");
@@ -148,12 +154,14 @@ int main (int argc, char *argv[]) {
     int* pStatus;                   // pointer to execution status
     int i;                          // counting variable
 
-    /* initializing the application defined thread id arrays for the producers and the consumers and the random number
-     generator */
+    n_files = argc - optind;
 
-    if ((t_worker_id = malloc(n_threads * sizeof(pthread_t))) == NULL
-            || (worker_id = malloc(n_threads * sizeof(unsigned int))) == NULL
-            || (statusWorkers = malloc(n_threads * sizeof(unsigned int))) == NULL) {
+    /* Initializing the internal and application-defined thread id arrays, thread status array and word counters */
+
+    if ((t_worker_id = malloc(n_threads * sizeof(pthread_t))) == NULL       ||
+        (worker_id = malloc(n_threads * sizeof(unsigned int))) == NULL      ||
+        (statusWorkers = malloc(n_threads * sizeof(unsigned int))) == NULL  ||
+        (counters = (int (*)[N_VOWELS])calloc(n_files, sizeof(*counters)) == NULL)) {
         fprintf(stderr, "error on allocating space to both internal / external worker id arrays\n");
         exit(EXIT_FAILURE);
     }
@@ -169,62 +177,10 @@ int main (int argc, char *argv[]) {
         }
     }
 
-    pthread_cond_init(&await_work, NULL);
-    pthread_cond_init(&await_results, NULL);
-
-    for (; optind < argc; optind++) {
-        char* file_name = argv[optind];
+    for (i = 0; i < n_files; i++) {
+        char* file_name = argv[optind+i];
 		printf("File name: %s\n", file_name);
-        
-        // Assign Work
-        if (pthread_mutex_lock (&accessCR) != 0) {                  // Enter Monitor
-            fprintf(stderr, "error on entering monitor");
-            exit(EXIT_FAILURE);
-        }
-        file_pointer = fopen(file_name, "r");                       // Next File to process
-        for (i = 0; i < N_VOWELS; i++)
-            counters[i] = 0;
-        
-        if (pthread_cond_broadcast(&await_work) != 0) {             // Signal workers to start processing file                                            
-            fprintf(stderr, "error on signaling await work");
-            exit(EXIT_FAILURE);
-        }
-        if (pthread_mutex_unlock (&accessCR) != 0) {                // Exit Monitor
-            fprintf(stderr, "error on exiting monitor");
-            exit(EXIT_FAILURE);
-        }
-
-
-        // Wait for Results
-        if (pthread_mutex_lock (&accessCR) != 0) {                  // Enter Monitor
-            fprintf(stderr, "error on entering monitor");
-            exit(EXIT_FAILURE);
-        }
-        if (pthread_cond_wait(&await_results, &accessCR) != 0) {    // Wait for workers to finish file processing
-            fprintf(stderr, "error on waiting for signal await results");
-            exit(EXIT_FAILURE);
-        }
-        printResults();                                             // Print Results
-
-        if (pthread_mutex_unlock (&accessCR) != 0) {                // Exit Monitor
-            fprintf(stderr, "error on exiting monitor");
-            exit(EXIT_FAILURE);
-        }
-
-    }
-
-    if (pthread_mutex_lock (&accessCR) != 0) {                  // Enter Monitor
-        fprintf(stderr, "error on entering monitor");
-        exit(EXIT_FAILURE);
-    }
-    work_done = true;
-    if (pthread_cond_broadcast(&await_work) != 0) {             // Signal workers to start processing file                                            
-        fprintf(stderr, "error on signaling await work");
-        exit(EXIT_FAILURE);
-    }
-    if (pthread_mutex_unlock (&accessCR) != 0) {                // Exit Monitor
-        fprintf(stderr, "error on exiting monitor");
-        exit(EXIT_FAILURE);
+        file_names[i] = file_name;                       // Next File to process
     }
 
     for (i = 0; i < n_threads; i++) {
@@ -233,88 +189,42 @@ int main (int argc, char *argv[]) {
             exit(EXIT_FAILURE);
         }
     }
+
+    printResults();
+    
     free(t_worker_id);
     free(worker_id);
 
     return EXIT_SUCCESS;
 }
 
-
+int start_working = 0;
 void *worker(void *par) {
     unsigned int id = *((unsigned int *) par);
     printf("Init worker %d\n", id);
 
-    int worker_counters[N_VOWELS] = {0,0,0,0,0,0,0};              // Internal Counters
+    int (*worker_counters)[N_VOWELS];              // Internal Counters
+    int worker_file_id;
     char *chunk;
-    int chunk_size;
+    int chunk_size = 2;
     
-    if ((chunk = malloc(chunk_size*1024 * sizeof(char))) == NULL ) {
+    if ((chunk = malloc(max_chunk_size*1024 * sizeof(char))) == NULL ||
+        (worker_counters = (int (*)[N_VOWELS])calloc(n_files, sizeof(*worker_counters)) == NULL)) {
         perror("error on allocating space to both internal / external worker id arrays\n");
         statusWorkers[id] = EXIT_FAILURE;
         pthread_exit(&statusWorkers[id]);
     }
 
     while (!work_done) {
-
-        while (!file_done) {
-            // Get chunks (critical region)
-            if (pthread_mutex_lock(&accessCR) != 0) {              // Enter Monitor
-                perror("error on entering monitor");
-                statusWorkers[id] = EXIT_FAILURE;
-                pthread_exit(&statusWorkers[id]);
-            }
-            readChunk(chunk, &chunk_size);
-            file_done = chunk_size == 0;
-            if (pthread_mutex_unlock(&accessCR) != 0) {            // Exit Monitor
-                perror("error on exit monitor");
-                statusWorkers[id] = EXIT_FAILURE;
-                pthread_exit(&statusWorkers[id]);
-            }
-            // Can be done in parallel
-            processText(chunk, chunk_size, worker_counters);
-        }
-        
-        if (pthread_mutex_lock(&accessCR) != 0) {                  // Enter Monitor
-            perror("error on entering monitor");
-            statusWorkers[id] = EXIT_FAILURE;
-            pthread_exit(&statusWorkers[id]);
-        }
-        updateCounters(worker_counters);
-        finished_processing++;
-
-        if (finished_processing == n_threads) {
-            if (pthread_cond_signal(&await_results) != 0) {        // Signal main to advance to next file, or terminate                                            
-                perror("error on signaling await results");
-                statusWorkers[id] = EXIT_FAILURE;
-                pthread_exit(&statusWorkers[id]);
-            }
-            finished_processing = 0;
-        }
-
-        if (pthread_mutex_unlock(&accessCR) != 0) {                // Exit Monitor
-            perror("error on exit monitor");
-            statusWorkers[id] = EXIT_FAILURE;
-            pthread_exit(&statusWorkers[id]);
-        }
-
-        // Wait for main's next signal to work
-        if (pthread_mutex_lock(&accessCR) != 0) {                  // Enter Monitor
-            perror("error on entering monitor");
-            statusWorkers[id] = EXIT_FAILURE;
-            pthread_exit(&statusWorkers[id]);
-        }
-        if ((pthread_cond_wait(&await_work, &accessCR)) != 0) { 
-            perror ("error on waiting for work to do");
-            statusWorkers[id] = EXIT_FAILURE;
-            pthread_exit(&statusWorkers[id]);
-        }
-        if (pthread_mutex_unlock(&accessCR) != 0) {                // Exit Monitor
-            perror("error on exit monitor");
-            statusWorkers[id] = EXIT_FAILURE;
-            pthread_exit(&statusWorkers[id]);
-        }
+        // Get chunks (critical region)
+        readChunk(id, chunk, worker_file_id);
+        // Can be done in parallel
+        processText(chunk, chunk_size, worker_counters);
     }
+    // Update word counts (critical region)
+    updateCounters(id, worker_counters, worker_file_id);
 
+    free(worker_counters);
     free(chunk);
 
     statusWorkers[id] = EXIT_SUCCESS;
@@ -327,21 +237,28 @@ void processText(char* chunk, int chunk_size, int* worker_counters) {
         worker_counters[i] = 1;
 }
 
-int readChunk(char* chunk, int* chunk_size) {
+int readChunk(unsigned int worker_id, unsigned char* chunk, int* file_id) {
     printf("Read Chunk\n");
-    *chunk_size = 0;
+    if(pthread_mutex_lock(&accessCR)) {
+        perror ("error on entering monitor(CF)");
+        statusWorkers[worker_id] = EXIT_FAILURE;
+        pthread_exit (&statusWorkers[worker_id]);   
+    }
+    *file_id = 1;
+    if(pthread_mutex_unlock(&accessCR)) {
+        perror ("error on entering monitor(CF)");
+        statusWorkers[worker_id] = EXIT_FAILURE;
+        pthread_exit (&statusWorkers[worker_id]);    
+    }
     return 1;
 }
 
-void updateCounters(int* worker_counters) {
+void updateCounters(unsigned int worker_id, int* worker_counters, int* file_id) {
     printf("Update Counters\n");
-    for (int i = 0; i < N_VOWELS; i++) {
-        counters[i] += worker_counters[i];
-        worker_counters[i] = 0;
-    }
 }
 
-void printResults() {
+
+void printResults(char** file_names) {
     printf("Total number of words = %d\n", counters[0]);
     printf("N. of words with an\n");
     printf("      A\t    E\t    I\t    O\t    U\t    Y\n  ");
@@ -352,7 +269,7 @@ void printResults() {
 }
 
 static void print_usage (char *cmdName) {
-  fprintf (stderr, "\nSynopsis: %s [OPTIONS] FILE...\n"
+    fprintf(stderr, "\nSynopsis: %s [OPTIONS] FILE...\n"
            "  OPTIONS:\n"
            "  -h      --- print this help\n", cmdName);
 }
